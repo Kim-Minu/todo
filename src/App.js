018fe33eff3b270e0dfcf040a66963024e0bb16f
minwoo <minu0922@gmail.com>
2023-07-19 21:53:57 +0900

    api call 셋팅

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
 import Todo from './Todo';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Container, List, Paper} from "@mui/material"
 import AddTodo from './AddTodo';
+import {call} from './ApiService';
 
 function App() {
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+      call("/todo", "GET", null)
+      .then((response) => setItems(response.data));
+  }, []);
+
   const addItem = (item) => {
+    call("/todo", "POST", item)
+    .then((response) => setItems(response.data));
+  };
+
+  const deletItem = (item) => {
+    call("/todo", "DELETE", item)
+    .then((response) => {
+      const newItmes = items.filter(e => e.id !== item.id);
+      setItems([...newItmes]);
+    });
+
+  };
+
+  const editItem = (item) => {
+    call("/todo", "PUT", item)
+    .then((response) => setItems(response.data));
+  };
+
+  /*const addItem = (item) => {
     item.id = "ID-" + items.length;
     item.done = false;
     setItems([...items, item]);
@@ -23,7 +48,7 @@ function App() {
 
   const editItem = (item) => {
     setItems([...items]);
-  }
+  }*/
 
   let todoItems = items.length > 0 && (
     <Paper style={{margin: 16}}>
---
+++ src/login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+    return <p>로그인 페지</p>
+}
+
+export default Login;
